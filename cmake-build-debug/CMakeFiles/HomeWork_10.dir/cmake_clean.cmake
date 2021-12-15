2ee12b42d8680fc0da4777d7d64f5c0ae394bc93
Maxim Akimenko <maxnet663@gmail.com>
2021-12-15 18:14:40 +0300

    Source

---
+++ cmake-build-debug/CMakeFiles/HomeWork_10.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork_10.dir/main.cpp.obj"
+  "HomeWork_10.exe"
+  "HomeWork_10.exe.manifest"
+  "HomeWork_10.lib"
+  "HomeWork_10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeWork_10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
